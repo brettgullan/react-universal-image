2e4db2d9c0207182771459029e65ce63294007e8
Brett Gullan <brettgullan@mac.com>
2019-07-29 15:38:30 +1000

    chore: Turn off default stories (remove later)

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button, Welcome } from '@storybook/react/demo'
 
+/*
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
@@ -17,3 +18,4 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+*/
