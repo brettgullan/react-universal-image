a3b2c6386093e47ee3f548db75941d2377ecc8cc
Brett Gullan <brettgullan@mac.com>
2019-07-30 06:54:20 +1000

    feat: Add neutrino build support for styled components’ css prop

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,3 +1,19 @@
 module.exports = {
-  use: ['@neutrinojs/react-components']
+  use: [
+    '@neutrinojs/react-components',
+    (neutrino) => {
+      neutrino.config.module
+        .rule('compile')
+        .use('babel')
+        .tap((options) => {
+          const babelStyledComponentsPlugin = [
+            require.resolve('babel-plugin-styled-components'),
+            {},
+          ]
+          options.plugins.push(babelStyledComponentsPlugin)
+          console.log(options)
+          return options
+        })
+    },
+  ],
 }
